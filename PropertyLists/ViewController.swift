5f01f981d7ad725e61a1132a93e86bf8122ece02
davidlopez <dfl11@alu.ua.es>
2023-02-04 21:43:50 +0100

    Ejercicio Property lists

--- PropertyLists/ViewController.swift
+++ PropertyLists/ViewController.swift
@@ -7,11 +7,33 @@
 
 import UIKit
 
+struct Alumno : Codable {
+    var nombre : String
+    var notas : [Float]
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let fileManager = FileManager.default
+        let documentsDirectory = fileManager.urls(for:.documentDirectory,in:.userDomainMask)[0]
+        let urlPlist = documentsDirectory.appendingPathComponent("alumnos.plist")
+
+        let encoder = PropertyListEncoder()
+        encoder.outputFormat = .xml
+
+        let a1 = Alumno(nombre: "Pepe", notas: [5, 8.5, 10])
+        let a2 = Alumno(nombre: "Eva", notas: [10, 9])
+        let alumnos = [a1,a2]
+
+        do {
+          let data = try encoder.encode(alumnos)
+          try data.write(to: urlPlist)
+        } catch {
+          print(error)
+        }
+
     }
 
 
